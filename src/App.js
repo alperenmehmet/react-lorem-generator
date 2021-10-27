ee7943019fcec86fdd0700be1d0d4131692ab09b
Mehmet Alperen <me.alperen@gmail.com>
2021-10-27 10:30:15 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
 
-function App() {
+const url = 'https://api.chucknorris.io/jokes/random';
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div>
+        <h2 className='title'>LOREM IPSUM GENERATOR</h2>
+        <form action='' onSubmit={handleSubmit}>
+          <label htmlFor=''>Paraghraphs : </label>
+          <input
+            type='number'
+            name='amount'
+            id='number'
+            onChange={handleChange}
+          />
+          <button className='btn'>Generate</button>
+        </form>
+        <div className='lorem-paraghraphs'>
+          {text.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
 export default App;
